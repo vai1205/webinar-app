fc836c5ca2279455d64d4f04c054d4f54c804f21
vai1205 <vaibhav.12may95@gmail.com>
2020-08-29 12:30:38 +0530

    team deails popup mobile responsive

--- src/components/popupModal.js
+++ src/components/popupModal.js
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Popup from "reactjs-popup";
-import {FiArrowRight} from 'react-icons/fi';
 
 const PopupModal = ({ title, img, content }) => {
   const [isOpen, setIsOpen] = useState(true);
-  useEffect(()=>{
+  useEffect(() => {
     setIsOpen(true);
   })
-  const opener = () => {
-    return(
-      <>
-        <button>
-        <><div>Read More..</div><FiArrowRight/></>
-        </button>
-      </>
-    )
-  }
+
   return (
     <>
       {
         isOpen &&
-        <Popup trigger={<div className="read-more-modal">Read More &rarr;</div>} modal>
+        <Popup trigger={<div className="read-more-modal">Read More &rarr;</div>} modal lockScroll>
           <div className="popup-modal">
             <a className="close-modal" onClick={() => { setIsOpen((currentState) => !currentState) }}>
               &times;
